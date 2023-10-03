2aa90eceb62fb89cf9f80c44e21a422c660d9bb3
Ekhfa Hossain <ekhfahossain@gmail.com>
2023-10-03 20:02:12 +0600

    Created Fronend for ListProducts

--- grocery-fetch/app/page.jsx
+++ grocery-fetch/app/page.jsx
@@ -1,11 +1,10 @@
 import React from "react";
+import axios from "axios";
+import ListProducts from "@/components/products/ListProducts";
+import productsData from "@/components/products/products";
 
-const page = () => {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
-    </div>
-  );
+const Homepage = () => {
+  return <ListProducts data={productsData} />;
 };
 
-export default page;
+export default Homepage;
--- grocery-fetch/components/products/ListProducts.jsx
+++ grocery-fetch/components/products/ListProducts.jsx
@@ -1,21 +1,31 @@
-"use client";
-
 import React from "react";
 import Filters from "./layouts/Filter";
+import productsData from "./products.json";
 
 const ListProducts = () => {
-  //console.log(data);
   return (
     <section className="py-12">
       <div className="max-w-screen-xl container mx-auto px-4">
-        <div className="md:flex-row -mx-4 flex flex-col">
-          <Filters />
-
-          <main className="md:w-2/3 lg:w-3/4 px-3">
-            {data?.products?.map(product => (
-              <ProductItem key={product?._id} product={product} />
-            ))}
-          </main>
+        <div className="md:flex-row -mx-4 flex flex-wrap">
+          {productsData.map(product => (
+            <div
+              key={product.id}
+              className="bg-white md:w-1/2 lg:w-1/3 xl:w-1/4 mb-4 w-full px-4"
+            >
+              <div className="overflow-hidden rounded-lg shadow-lg">
+                <img
+                  src={`https://via.placeholder.com/300x200?text=Product+${product.id}`}
+                  alt={product.name}
+                  className="h-48 w-full object-cover"
+                />
+                <div className="p-4">
+                  <h2 className="text-xl font-semibold">{product.name}</h2>
+                  <p className="text-gray-600">{product.description}</p>
+                  <p className="mt-2 text-lg font-semibold">${product.price}</p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
